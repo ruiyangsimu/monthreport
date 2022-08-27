80e89b24be9bcf574b1d53b26921bb78f4d7dc08
jeremy <1565803020@qq.com>
2022-08-27 20:43:43 +0800

    增加本地打包虚拟环境，减少本地打包体积

--- build.ps1
+++ build.ps1
@@ -1,1 +1,7 @@
+# 创建虚拟环境，首次下载项目时打开注释执行，创建属于本项目的python环境，防止环境污染
+# virtualenv venv
+# 激活虚拟环境
+.\venv\Scripts\activate.ps1
+# 安装包
+pip install -r .\requirements.txt
 pyinstaller --clean --win-private-assemblies --key 0123456789 -F main.py
